2aa78337cfbf25a1365019a14a76d074b0800062
parfreyn24 <nparf12@yahoo.com>
2023-05-31 10:27:15 -0400

    learning how to commit

--- app/src/main/java/com/example/chemulator/AboutFragment.kt
+++ app/src/main/java/com/example/chemulator/AboutFragment.kt
@@ -3,12 +3,11 @@ package com.example.chemulator
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.chemulator.databinding.FragmentAboutBinding
-import com.example.chemulator.databinding.FragmentWaveBinding
 
 
 class AboutFragment : Fragment() {
@@ -22,17 +21,18 @@ class AboutFragment : Fragment() {
         _binding = FragmentAboutBinding.inflate(inflater, container, false)
         val rootView = binding.root
 
-binding.button.setOnClickListener{view ->
-    val intent = Intent(Intent.ACTION_SENDTO)
-    intent.data = Uri.parse("mailto:") // only email apps should handle this
-    intent.putExtra(Intent.EXTRA_EMAIL, "nparf12@gmail.com")
-    intent.putExtra(Intent.EXTRA_SUBJECT,"Feedback")
-    if (intent.resolveActivity(requireActivity().packageManager) != null) {
-        startActivity(intent)
-    }
+binding.button4.setOnClickListener{view ->
+     dialPhoneNumber("(570) 290-3489")
 }
         return rootView
     }
-
+    fun dialPhoneNumber(phoneNumber: String) {
+        val intent = Intent(Intent.ACTION_DIAL).apply {
+            data = Uri.parse("tel:$phoneNumber")
+        }
+        if (intent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivity(intent)
+        }
+    }
 
 }
